68bf1ccb279907b62fbcc9ea4a3edc84ec871c5d
Eitan-Sprejer <eitusprejer@gmail.com>
2023-09-28 22:06:11 -0300

    Fix: Remove try-except

--- image_preprocessing.py
+++ image_preprocessing.py
@@ -8,43 +8,55 @@ from PIL import Image
 import matplotlib.pyplot as plt
 import numpy as np
 
-def process_image(file_name, type = 'png', target_size=(100, 100), threshold = 90, invert = False):
-    try:
-        # Open the image using Pillow
-        image = Image.open(f'exp_pics/{file_name}.{type}')
+def process_image(file_name, image_type = 'png', target_size=(100, 100), threshold = 90, invert = False):
+    """
+    This function takes a png image and converts it to a binary image.
+    The image is first converted to grayscale, then resized to the desired size.
+    The image is then converted to a numpy array and saved as a binary image.
 
-        # Resize the image to the specified dimensions
-        image = image.resize(target_size, Image.ANTIALIAS)
+    Parameters
+    ----------
+    file_name : str
+        Name of the file to be processed.
+    image_type : str, optional
+        Type of the image. The default is 'png'.
+    target_size : tuple, optional
+        Size of the image after resizing. The default is (100, 100).
+    threshold : int, optional
+        Threshold value for the binary image. The default is 90.
+    invert : bool, optional
+        Whether to invert the image or not. The default is False.
+    """
+    # Open the image using Pillow
+    image = Image.open(f'exp_pics/{file_name}.{image_type}')
+    # Resize the image to the specified dimensions
+    image = image.resize(target_size, Image.ANTIALIAS)
 
-        # Convert the image to grayscale
-        image = image.convert('L')
+    # Convert the image to grayscale
+    image = image.convert('L')
 
-        # Convert the image to a NumPy array
-        image_array = np.array(image)
+    # Convert the image to a NumPy array
+    image_array = np.array(image)
 
-        #Generate Binary Image
-        binary_image = (image_array > threshold).astype(np.uint8)
+    #Generate Binary Image
+    binary_image = (image_array > threshold).astype(np.uint8)
 
-        #Invert the image if necesary 
-        if invert == True:
-            binary_image = 1 - binary_image
+    #Invert the image if necesary
+    if invert:
+        binary_image = 1 - binary_image
 
-        #plot both images to check if the threshold is good enough
-        plt.figure(figsize=(6, 6))
-        plt.suptitle('Image Preprocessing')
-        plt.subplot(2, 2, 1)
-        plt.imshow(image_array, cmap = 'gray')
-        plt.title("Image Array")
-        plt.subplot(2, 2, 2)
-        plt.imshow(binary_image, cmap='gray') 
-        plt.title(f"Binary Image - Threshold: {threshold}")
-        plt.show()
+    #plot both images to check if the threshold is good enough
+    plt.figure(figsize=(6, 6))
+    plt.suptitle('Image Preprocessing')
+    plt.subplot(2, 2, 1)
+    plt.imshow(image_array, cmap = 'gray')
+    plt.title("Image Array")
+    plt.subplot(2, 2, 2)
+    plt.imshow(binary_image, cmap='gray') 
+    plt.title(f"Binary Image - Threshold: {threshold}")
+    plt.show()
 
-        #binary_image_sv = Image.fromarray((binary_image*255).astype(np.uint8))
-        #binary_image_sv.save(f'processed_patterns/{file_name}_processed.png')
+    # binary_image_to_save = Image.fromarray((binary_image*255).astype(np.uint8))
+    # binary_image_to_save.save(f'processed_patterns/{file_name}_processed.png')
 
-        return binary_image
-
-    except Exception as e:
-        print(f"Error processing image {file_name}: {e}")
-        return None
+    return binary_image
