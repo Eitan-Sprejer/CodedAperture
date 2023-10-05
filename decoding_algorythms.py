8910b46fe971d3562b9c0d03d8bdf6d5ec0a2aeb
Eitan-Sprejer <eitusprejer@gmail.com>
2023-10-05 15:41:03 -0300

    Refactor code for decoding process

--- decoding_algorythms.py
+++ decoding_algorythms.py
@@ -1,10 +1,37 @@
-def mura_decoding_algorythm(mask_value: int, i: int, j: int):
-    """Algorythm for decoding a mura generated image in codedaperture"""
-    if i+j == 0:
-        ans = 1
+import numpy as np
+from scipy.ndimage import convolve
+from utils import SourceScreen, SlitScreen, SensorScreen
+
+def get_mura_decoding_pattern(slit: SlitScreen):
+    decoding_pattern = np.zeros_like(slit.mask)
+    for i in range(slit.mask.shape[0]):
+        for j in range(slit.mask.shape[1]):
+            cent_i = int(i - (slit.mask.shape[0]-1)/2)
+            cent_j = int(j - (slit.mask.shape[1]-1)/2)
+            if cent_i+cent_j == 0:
+                decoding_pattern[i, j] = 1
+            else:
+                if slit.mask[i, j] == 1:
+                    decoding_pattern[i, j] = 1
+                elif slit.mask[i, j] == 0:
+                    decoding_pattern[i, j] = -1
+    return decoding_pattern
+
+def get_general_decoding_pattern(slit: SlitScreen):
+    transparency = np.sum(slit.mask)/(slit.mask.shape[0]*slit.mask.shape[1])
+    decoding_pattern = np.zeros_like(slit.mask)
+    for i in range(slit.mask.shape[0]):
+        for j in range(slit.mask.shape[1]):
+            if slit.mask[i, j] == 1:
+                decoding_pattern[i, j] = 1
+            elif slit.mask[i, j] == 0:
+                decoding_pattern[i, j] = transparency/(transparency-1)
+    return decoding_pattern
+
+def decode_image(sensor: SensorScreen, slit: SlitScreen, mask_type: str):
+    if mask_type == 'mura':
+        decoding_pattern = get_mura_decoding_pattern(slit)
     else:
-        if mask_value == 1:
-            ans = 1
-        elif mask_value == 0:
-            ans = -1
-    return ans
+        decoding_pattern = get_general_decoding_pattern(slit)
+    reconstructed_image = convolve(sensor.screen, decoding_pattern, mode='wrap')
+    return decoding_pattern, reconstructed_image
