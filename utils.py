cadc43dca7d47a08f84bab1890d49edeb94e46be
Eitan-Sprejer <eitusprejer@gmail.com>
2023-11-16 16:54:23 -0300

    Incorporate differenciated resolution change

--- utils.py
+++ utils.py
@@ -48,6 +48,7 @@ def get_objects_from_config(config_path):
     sensor_type = config["sensor"]["type"]
     sensor_config = {
         "mask_size": config["sensor"]["mask_size"],
+        "mask_resolution": config["sensor"]["mask_resolution"],
         "exposure_time": config["sensor"]["exposure_time"],
         **config["sensor"][sensor_type],
     }
@@ -106,8 +107,12 @@ def zoom_out_image(image: np.ndarray, zoom_out_factor: float):
     for i in range(new_rows):
         for j in range(new_cols):
             block = image[
-                int(np.floor(i * zoom_out_factor)) : int(np.ceil((i + 1) * zoom_out_factor)),
-                int(np.floor(j * zoom_out_factor)) : int(np.ceil((j + 1) * zoom_out_factor)),
+                int(np.floor(i * zoom_out_factor)) : int(
+                    np.ceil((i + 1) * zoom_out_factor)
+                ),
+                int(np.floor(j * zoom_out_factor)) : int(
+                    np.ceil((j + 1) * zoom_out_factor)
+                ),
             ]
             downsampled_image[i, j] = np.mean(block)
 
@@ -125,6 +130,12 @@ def zoom_out_image(image: np.ndarray, zoom_out_factor: float):
 
     return padded_image
 
+def upsample_image(
+    image: np.ndarray, new_width: int, new_height: int
+):
+     return cv2.resize(
+         image, (new_width, new_height), interpolation=cv2.INTER_CUBIC
+     )
 
 def zoom_in_image(image: np.ndarray, zoom_in_factor: float):
     # Calculate the new size while maintaining the same resolution
@@ -132,8 +143,8 @@ def zoom_in_image(image: np.ndarray, zoom_in_factor: float):
     new_height = int(image.shape[0] * zoom_in_factor)
 
     # Resize the image using OpenCV
-    zoomed_image = cv2.resize(
-        image, (new_width, new_height), interpolation=cv2.INTER_CUBIC
+    zoomed_image = upsample_image(
+        image, new_width, new_height
     )
 
     # Calculate the cropping parameters with floating-point values
@@ -153,6 +164,7 @@ def zoom_in_image(image: np.ndarray, zoom_in_factor: float):
 
     return zoomed_image
 
+
 @dataclass
 class Decoder:
     decode_img: bool
@@ -166,6 +178,7 @@ class Decoder:
 
 @dataclass
 class SourceScreen:
+    mask_resolution: list
     mask_size: list
     mask_type: str
     mask_width: int
@@ -173,7 +186,9 @@ class SourceScreen:
 
     def __post_init__(self) -> None:
         self.mask_size = np.array(self.mask_size)
+        self.mask_resolution = np.array(self.mask_resolution)
         self.mask_generator = MaskGenerator(
+            mask_resolution=self.mask_resolution,
             mask_size=self.mask_size,
             mask_type=self.mask_type,
             mask_width=self.mask_width,
@@ -184,24 +199,31 @@ class SourceScreen:
 
 @dataclass
 class SlitScreen:
+    mask_resolution: list
     mask_size: list
     mask_type: str
     mask_width: int
     mura_config: dict
 
     def __post_init__(self) -> None:
+        self.mask_resolution = np.array(self.mask_resolution)
         self.mask_size = np.array(self.mask_size)
         self.mask_generator = MaskGenerator(
-            mask_size=self.mask_size,
+            mask_resolution=self.mask_resolution,
             mask_type=self.mask_type,
             mask_width=self.mask_width,
             mura_config=self.mura_config,
         )
         self.mask = self.mask_generator.generate_mask()
+        if np.logical_and(self.mask_type == "mura", np.any(self.mask_resolution != self.mask.shape)):
+            raise ValueError(
+                f"Mask resolution {self.mask_resolution} does not match the mask shape {self.mask.shape}"
+            )
 
 
 @dataclass
 class SensorScreen:
+    mask_resolution: list
     mask_size: list
     readout_noise: float
     dark_current: float
@@ -209,8 +231,9 @@ class SensorScreen:
 
     def __post_init__(self) -> None:
         self.mask_size = np.array(self.mask_size)
-        self.screen = np.zeros(self.mask_size)
-        self.noise_matrix = np.zeros(self.mask_size)
+        self.mask_resolution = np.array(self.mask_resolution)
+        self.screen = np.zeros(self.mask_resolution)
+        self.noise_matrix = np.zeros(self.mask_resolution)
         self.dark_current_noise = self.dark_current * self.exposure_time
 
 
@@ -222,7 +245,6 @@ class Options:
     source_to_slit_distance: float
     slit_to_sensor_distance: float
     field_of_view: dict
-    inter_pixel_distance: float
     theta_bounds: list
     phi_bounds: list
     automatic_angles: bool
@@ -251,14 +273,12 @@ class Options:
         # Set the maximum phi as the upper bound
         self.phi_bounds[1] = max_phi
 
-    def set_slit_to_sensor_distance(
-        self, slit_size: float, sensor_size: float
-    ):
+    def set_slit_to_sensor_distance(self, slit_size: float, sensor_size: float):
         """
         Automatically sets the sensor to slit distance so that the Field of View
         is set to the one passed in the experiment config file.
         """
-        theta = (self.field_of_view['fully_coded_field_of_view'] / 2) * (np.pi / 180)
+        theta = (self.field_of_view["fully_coded_field_of_view"] / 2) * (np.pi / 180)
         # If the sensor is smaller than the slit, the sensor is taken into account
         # to calculate the distance.
         if np.min(sensor_size) < np.min(slit_size):
@@ -267,41 +287,42 @@ class Options:
             self.slit_to_sensor_distance = (
                 slit_to_origin_distance - sensor_to_origin_distance
             )
-            self.source_to_sensor_distance = self.source_to_slit_distance + self.slit_to_sensor_distance
-        else:
-            self.slit_to_sensor_distance = (
-                np.min(slit_size) / (2 * np.tan(theta))
+            self.source_to_sensor_distance = (
+                self.source_to_slit_distance + self.slit_to_sensor_distance
             )
-            self.source_to_sensor_distance = self.source_to_slit_distance + self.slit_to_sensor_distance
+        else:
+            self.slit_to_sensor_distance = np.min(slit_size) / (2 * np.tan(theta))
+            self.source_to_sensor_distance = (
+                self.source_to_slit_distance + self.slit_to_sensor_distance
+            )
+
 
 def coordinates2positions(
-    mask_shape: np.ndarray, options: Options, coordinates: np.ndarray
+    mask_resolution: np.ndarray, mask_size: np.ndarray, coordinates: np.ndarray
 ):
     """
     Converts the coordinates of a pixel to its position in the source or slit plane.
     """
 
     return (
-        coordinates - mask_shape / 2
-    ) * options.inter_pixel_distance  # Center the position coordinates.
+        ((coordinates) - (mask_resolution - 1) / 2) * mask_size / (mask_resolution - 1)
+    )  # Center the position coordinates.
 
 
 def positions2coordinates(
-    mask_shape: np.ndarray, options: Options, positions: np.ndarray
+    mask_resolution: np.ndarray, mask_size: np.ndarray, positions: np.ndarray
 ):
     """
-    Converts the positions of a pixel to its coordinates in the source or slit plane.
+    Converts the position in the source or slit plane back to the coordinates of a pixel.
     """
-
     return (
-        (positions / options.inter_pixel_distance)  # Center the position coordinates.
-        + mask_shape / 2
+        (positions / (mask_size / (mask_resolution - 1))) + (mask_resolution - 1) / 2
     ).astype(int)
 
 
 class MaskGenerator:
     def __init__(self, **mask_config):
-        self.mask_size = mask_config["mask_size"]
+        self.mask_resolution = mask_config["mask_resolution"]
         self.mask_width = mask_config["mask_width"]
         self.mask_type = mask_config["mask_type"]
         if self.mask_type == "mura":
@@ -329,7 +350,7 @@ class MaskGenerator:
 
     def generate_phi_mask(self):
         """Generates a mask with a phi shape"""
-        height, length = self.mask_size
+        height, length = self.mask_resolution
         width = self.mask_width
         mask = np.zeros((height, length))
 
@@ -353,7 +374,7 @@ class MaskGenerator:
 
     def generate_pinhole_mask(self):
         """Generates a mask with a pinhole shape"""
-        height, length = self.mask_size
+        height, length = self.mask_resolution
         mask = np.zeros((height, length))
         hole_radius = int(self.mask_width / 2)
         # Define the lambda shape
@@ -365,7 +386,7 @@ class MaskGenerator:
 
     def generate_frame_mask(self):
         """Generates a mask with a square frame shape"""
-        height, length = self.mask_size
+        height, length = self.mask_resolution
         mask = np.zeros((height, length))
         slit_outer_radius = int((np.min([height, length]) + self.mask_width) / 2)
         mask[
@@ -404,9 +425,9 @@ class MaskGenerator:
 
         mask = image[:, :, 3]
         # Check that the size is the correct one
-        if not np.array_equal(mask.shape, self.mask_size):
+        if not np.array_equal(mask.shape, self.mask_resolution):
             raise ValueError(
-                f"Mask size {mask.shape} does not match the specified size {self.mask_size}"
+                f"Mask size {mask.shape} does not match the specified size {self.mask_resolution}"
             )
         return mask
 
@@ -421,16 +442,16 @@ class MaskGenerator:
         # Check if the processed image is already saved
         # if os.path.isfile(path):
         # mask = np.array(Image.open(path))
-        # if mask.shape != self.mask_size:
+        # if mask.shape != self.mask_resolution:
         #    raise ValueError(
-        #        f"Mask size {mask.shape} does not match the specified size {self.mask_size}"
+        #        f"Mask size {mask.shape} does not match the specified size {self.mask_resolution}"
         #    )
         # return mask
         # Load the png image as a mask matrix
 
         try:
             mask = process_image(
-                file_name=file_name, target_size=self.mask_size, invert=True
+                file_name=file_name, target_size=self.mask_resolution, invert=True
             )
         except FileNotFoundError as exc:
             raise FileNotFoundError(
